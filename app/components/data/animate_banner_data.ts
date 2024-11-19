8f51eda199cb00a0c1ff4f169cc0970e8191e540
Cebab1c905@3 <khishigtogtokh.hafel@gmail.com>
2024-11-19 09:09:46 +0800

    newAnimateLogoAdd

--- app/components/data/animate_banner_data.ts
+++ app/components/data/animate_banner_data.ts
@@ -3,7 +3,7 @@ import animateLogo2 from "@/app/LogoAnimatePagesLogos/AnimateLogo2.png";
 import animateLogo3 from "@/app/LogoAnimatePagesLogos/AnimateLogo3.png";
 import animateLogo4 from "@/app/LogoAnimatePagesLogos/AnimateLogo4.png";
 import animateLogo5 from "@/app/LogoAnimatePagesLogos/AnimateLogo5.png";
-
+import animateLogo6 from "@/app/LogoAnimatePagesLogos/AnimateLogo6.png";
 type AnimateBannerDataType = {
   id: number;
   img: string;
@@ -30,6 +30,10 @@ const animate_banner_data = (): AnimateBannerDataType[] => {
     {
       id: 5,
       img: animateLogo5.src,
+    },
+    {
+      id: 6,
+      img: animateLogo6.src,
     },
   ];
 };
