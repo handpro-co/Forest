797d3e5686ea49f53a38e026db47e708d656cd32
Hishgee2170 <hishgeehishge21@gmail.com>
2024-12-11 15:33:08 +0800

    Open AI Connected

--- app/components/layout/NewsCard.tsx
+++ app/components/layout/NewsCard.tsx
@@ -7,15 +7,15 @@ type NewsCardProps = {
 };
 const NewsCard: React.FC<NewsCardProps> = ({ image, date, title }) => {
   return (
-    <div className="flex flex-col gap-[25px] p-[8px] rounded-[20px] cursor-pointer hover:bg-[#ECEBE3] ">
+    <div className="flex flex-col gap-[25px] p-[8px] rounded-[20px] cursor-pointer hover:bg-[#ECEBE3]">
       <img src={image} className="rounded-[16px]" alt="billion tree" />
       <div className="gap-[16px] flex flex-col pb-[12px] pl-[8px]">
         <div className="text-[#666] text-[14px]">{date}</div>
         <div className="flex gap-[12px] items-center justify-between">
-          <div className="text-[#333] text-[16px] break-words max-w-[85%]">
-            {title}
+          <div className="text-[#333] text-[16px] break-words max-w-[85%] overflow-hidden">
+            <div className="line-clamp-3">{title}</div>
           </div>
-          <div className="w-[32px] h-[32px] bg-[#DEFF94] rounded-[50%] flex justify-center items-center">
+          <div className="min-w-[32px] min-h-[32px] bg-[#DEFF94] rounded-[50%] flex justify-center items-center">
             <ArrowRigth color="#333" />
           </div>
         </div>
