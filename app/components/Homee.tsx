824d3d8205531806952eda53004364e7c3ae19ac
Hishgee2170 <hishgeehishge21@gmail.com>
2025-01-10 14:02:19 +0800

    final03

--- app/components/Homee.tsx
+++ app/components/Homee.tsx
@@ -1,8 +1,9 @@
 "use client";
 import ArrowRigth from "../icons/ArrowRigth";
-import photo from "./homePhoto.png";
+import { AiOutlineSound } from "react-icons/ai";
+
 import HomeeText from "./layout/HomeeText";
-import { useEffect , useState } from "react";
+import { useEffect, useState } from "react";
 const data = [
   {
     title: "220 тэрбум",
@@ -19,8 +20,8 @@ const data = [
 ];
 
 const Homee: React.FC = () => {
-  const [videoSrc, setVideoSrc] = useState("");
-
+  const [videoSrc, setVideoSrc] = useState<string>("");
+  const [mute, setMute] = useState<boolean>(false);
   useEffect(() => {
     const getVideoByTime = () => {
       const currentHour = new Date().getHours();
@@ -36,34 +37,39 @@ const Homee: React.FC = () => {
 
     setVideoSrc(getVideoByTime());
   }, []);
+
   return (
     <>
-      <video
-        autoPlay
-        loop
-        className="absolute top-0 left-0 w-full h-[100vh] object-cover z-[-1]"
-      >
-        <source src="/backgroundVideo/01.mp4" type="video/mp4" />
-      </video>
+      {videoSrc && (
+        <video
+          autoPlay
+          loop
+          muted={mute}
+          className="absolute top-0 left-0 w-full h-[100vh] object-cover z-[-1]"
+        >
+          <source src={videoSrc} type="video/mp4" />
+        </video>
+      )}
 
-      <div
-        className="flex flex-col justify-center items-center lg:items-start w-full h-[90vh] bg-cover bg-center rounded-[24px] px-0 md:px-[56px] py-6 md:py-12 md:px-12 gap-[72px] md:gap-12"
-        // style={{
-        //   backgroundImage: `url(${photo.src})`,
-        // }}
-      >
-        <div className="text-center  lg:text-left text-white font-bold text-[32px] md:text-[52px] lg:text-[48px] xl:text-[56px] w-full md:w-2/3 xl:w-2/5 2xl:w-2/5">
+      <div className="flex flex-col justify-center items-center lg:items-start w-full h-[90vh] bg-cover bg-center rounded-[24px] px-0 md:px-[56px] py-6 md:py-12 md:px-12 gap-[72px] md:gap-12">
+        <div className="text-center lg:text-left text-white font-bold text-[32px] md:text-[52px] lg:text-[48px] xl:text-[56px] w-full md:w-2/3 xl:w-2/5 2xl:w-2/5">
           Амьдралын орчинг сайжруулна
         </div>
-
-        {/* Data Section */}
+        {/* bugtai baij boloh button */}
+        <button
+          onClick={() => {
+            setMute(!mute);
+          }}
+          className="fixed flex items-center justify-center bottom-[20px] left-[20px] h-[40px] w-[40px] rounded-full scale-110 z-5"
+        >
+          <AiOutlineSound />
+        </button>
         <div className="w-full flex flex-wrap items-start justify-center mt-6 gap-4 md:mt-8 md:flex-row md:flex-wrap md:justify-start">
           {data.map((item, index) => (
             <HomeeText key={index} title={item.title} text={item.text} />
           ))}
         </div>
 
-        {/* Button Section */}
         <div className="mt-6">
           <button className="bg-[#14B75F] flex items-center justify-center px-6 py-2 rounded-full gap-4 shadow-md hover:bg-[#12a150] transition-all">
             <span className="text-white font-medium text-lg">
@@ -78,5 +84,4 @@ const Homee: React.FC = () => {
     </>
   );
 };
-
 export default Homee;
